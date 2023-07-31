a18a928c0d1bc97f564385ad66975026a7c1c784
FriendSkullaton <121736833+FriendSkullaton@users.noreply.github.com>
2023-07-31 13:35:19 +1000

    badabing overhaul

--- scripts.js
+++ scripts.js
@@ -51,3 +51,38 @@ xhttp.onreadystatechange = function() {
 };
 xhttp.open("GET", "https://weirdscifi.ratiosemper.com/neocities.php?sitename=skull-aton", true);
 xhttp.send();
+
+// Gallery and Modal
+
+let imageBox1 = document.getElementById("imageBox1");
+
+function galleryFunction1(smallImg) {
+  imageBox1.src = smallImg.src;
+}
+
+// Get the modal image tag
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+var modalImage = document.getElementById("modal-image");
+
+// When the user clicks the big picture, set the image and open the modal
+imageBox1.onclick = function (e) {
+  var src = e.srcElement.src;
+  modal.style.display = "block";
+  modalImage.src = src;
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
